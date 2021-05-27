45c38fa606c64c1712709b53a2cbeeb4e92997e0
David <yao.yi.18f@szlgbp.hu>
2021-05-27 11:38:10 +0200

    public

--- server.js
+++ server.js
@@ -2,8 +2,5 @@ const express = require('express');
 
 const app = express();
 
-app.get("/", function (request, response) {
-    response.send("Hello world!");
-});
-
+app.use(express.static("public"));
 app.listen(9000);
