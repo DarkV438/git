53e6c477ac9c66d632c8917f1426e7f54be5a2dd
David <yao.yi.18f@szlgbp.hu>
2021-06-03 11:27:01 +0200

    git

--- server.js
+++ server.js
@@ -1,12 +1,34 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const config = require('./config.json');
+
+mongoose.connect(config.databadeURL);
 
 const app = express();
 
-app.use(express.static("public"));
+app.use(express.static('public'));
+
+
+
+const schema = new mongoose.Schema({
+    new: String,
+    szavazatok: Number
+});
+
+const model = mongoose.model('Opciok',schema ,'Opciok');
+
 app.use(express.urlencoded());
+
 app.post("/szavazas", function(request, response) {
     console.log(request.body);
 
+    new model({
+
+        nev: request.body.animals,
+        szavazatok: 1
+    }).save();
+
     response.redirect("/");
 });
+
 app.listen(9000);
