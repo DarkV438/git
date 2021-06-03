b3094011620fbeac413e9ce30d904539e152e665
David <yao.yi.18f@szlgbp.hu>
2021-06-03 10:43:37 +0200

    2

--- server.js
+++ server.js
@@ -3,4 +3,10 @@ const express = require('express');
 const app = express();
 
 app.use(express.static("public"));
+app.use(express.urlencoded());
+app.post("/szavazas", function(request, response) {
+    console.log(request.body);
+
+    response.redirect("/");
+});
 app.listen(9000);
